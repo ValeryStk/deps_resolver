f8bdd346f5814e2314a290d838e858b23cd2eb9d
ValeryStk <tor-tech@mail.ru>
2024-09-25 11:50:43 +0300

    WIP (test)

--- deps/copy_all_deps.py
+++ deps/copy_all_deps.py
@@ -1,3 +1,11 @@
+# SCRIPT FOR RESOLVING ALL DEPENDENCIES
+"""
+
+@Author: Valery Stanchyk
+@Date: 2024/09/25
+
+"""
+
 import os
 import shutil
 import py7zr
@@ -5,15 +13,17 @@ import py7zr
 # CONSTANTS
 DEPS_EXE_FILE = "deps_exe"
 DEPS_ONE_LEVEL_ABOVE_EXE_FILE = "deps_above_exe"
+DEPS_ONLY_FOR_DEBUG = "deps_only_for_debug"
+DEPS_ONLY_FOR_RELEASE = "deps_only_for_release"
 RELEASE_DIR_NAME = "release"
 DEBUG_DIR_NAME = "debug"
 LEVELS_UP_TO_BUILD_DIRS = "../../"
 ARHIV_FILES = [".7z","zip","rar"]
 
-# ARHIV UNPACKED FILES FOR DELETING AFTER RESOLVING DEPS
-unpacked_folders = []
+# ARRAY FOR OUT DIRS
+out_dir_list = []
 
-# FUNCTIONS
+# COMMON FUNCTIONS
 def copytree(src, dst, symlinks=False, ignore=None):
     for item in os.listdir(src):
         s = os.path.join(src, item)
@@ -46,19 +56,51 @@ def unpack(file,out):
    archive.extractall(out)
    archive.close() 
 
+def resolve_deps(files_list, DEPS_FOLDER_NAME:str, IS_EXE_LEVEL:bool):
+ for file in files_list:
+     file = DEPS_FOLDER_NAME + "/" + file
+     print(file)
+     for out in out_dir_list:
+        r_d = ""
+        if IS_EXE_LEVEL:
+           r_d = make_release_or_debug_path(out)
+           if r_d == '/':
+              print('RELEASE DEBUG UNEXPECTED ERROR',out)
+              continue
+           if DEPS_FOLDER_NAME == DEPS_ONLY_FOR_RELEASE:
+               if r_d == '/debug':
+                   continue
+           if DEPS_FOLDER_NAME == DEPS_ONLY_FOR_DEBUG:
+               if r_d == '/release':
+                   continue
+        if os.path.isfile(file):
+           if is_file_arhiv (file):
+              unpack(file, LEVELS_UP_TO_BUILD_DIRS + out + r_d)
+              continue
+           print("file exists: " + file)
+           print(f"copy exe file deps {file} to: -->", LEVELS_UP_TO_BUILD_DIRS + out + r_d)
+           shutil.copy(file, LEVELS_UP_TO_BUILD_DIRS + out + r_d)
+        if os.path.isdir(file):
+           file_name = LEVELS_UP_TO_BUILD_DIRS + out + r_d
+           print(f"copy dir deps {file} to: --> {file_name}")
+           base_name_ = os.path.basename(file)
+           print("BASE NAME: -- >" + base_name_)
+           full_path = file_name + "/" + base_name_
+           if not os.path.exists(full_path):
+               os.mkdir(full_path)
+           copytree(file, full_path)
 
-# SCRIPT FOR RESOLVING ALL DEPENDENCIES
+
+# PREPARATIONS ########################################################################################################
+
 print('STEP 1 - Create deps directories list and files list')
 files_in_exe_list = os.listdir(DEPS_EXE_FILE)
-for file in files_in_exe_list:
-    print(file)
 files_in_above_exe_list = os.listdir(DEPS_ONE_LEVEL_ABOVE_EXE_FILE)
-for file in files_in_above_exe_list:
-    print(file)
+files_only_for_debug = os.listdir(DEPS_ONLY_FOR_DEBUG)
+files_only_for_release = os.listdir(DEPS_ONLY_FOR_RELEASE)
 
 
 print('\nSTEP 2 - Create build directories list')
-out_dir_list = []
 build_dirs_list = os.listdir(LEVELS_UP_TO_BUILD_DIRS)
 for dir in build_dirs_list:
     if not RELEASE_DIR_NAME  in dir.lower() and not DEBUG_DIR_NAME in dir.lower():
@@ -77,9 +119,10 @@ for dir in build_dirs_list:
     out_dir_list.append(dir)
 
 
+# RESOLVING DEPS SECTION ##############################################################################################
+
 print('STEP 3 - Copy files to build dirs with .EXE')
-print(out_dir_list)
-for file in files_in_exe_list:
+'''for file in files_in_exe_list:
     file = DEPS_EXE_FILE + "/" + file
     print(file)
     for out in out_dir_list:
@@ -102,11 +145,12 @@ for file in files_in_exe_list:
            full_path = file_name + "/" + base_name_
            if not os.path.exists(full_path):
                os.mkdir(full_path)
-           copytree(file, full_path)
+           copytree(file, full_path)'''
+resolve_deps(files_in_exe_list, DEPS_EXE_FILE, True)
 
 
 print('STEP 4 - Copy files to build one level above exe dir')
-for file in files_in_above_exe_list:
+'''for file in files_in_above_exe_list:
     file = DEPS_ONE_LEVEL_ABOVE_EXE_FILE + "/" + file
     for out in out_dir_list:
         if os.path.isfile(file):
@@ -123,5 +167,14 @@ for file in files_in_above_exe_list:
             full_path = file_name + "/" + base_name_
             if not os.path.exists(full_path):
                os.mkdir(full_path)
-            copytree(file, full_path)
+            copytree(file, full_path)'''
+resolve_deps(files_in_above_exe_list, DEPS_ONE_LEVEL_ABOVE_EXE_FILE, False)
+
+
+print('STEP 5 - Copy files only for debug to exe dir')
+resolve_deps(files_only_for_debug, DEPS_ONLY_FOR_DEBUG, True)
+
+
+print('STEP 6 - Copy files only for release to exe dir')
+resolve_deps(files_only_for_release, DEPS_ONLY_FOR_RELEASE, True)
 
